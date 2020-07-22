0885179f5680fe588acdfa60bb82b0891c612655
ATCamposs <andre.tatibano@gmail.com>
2020-07-22 19:43:15 -0300

    Desenvolvendo detalhes repositorios

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,14 +1,69 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom'
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
- repository: string;
-}
+  repository: string;
+ }
 
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-return <h1>Repository: {params.repository}</h1>;
+return (
+  <>
+  <Header>
+    <img src={logoImg} alt="Github Explorer" />
+  <Link to="/dashboard">
+      <FiChevronLeft size={16} />
+    Voltar
+  </Link>
+  </Header>
+
+  <RepositoryInfo>
+    <header>
+      <img src="http://google.com" alt="Google inc" />
+      <div>
+        <strong>
+          rocketseat/unform
+        </strong>
+        <p>
+          descrição do repo
+        </p>
+      </div>
+    </header>
+    <ul>
+      <li>
+        <strong>1080</strong>
+        <span>Stars</span>
+      </li>
+      <li>
+        <strong>20</strong>
+        <span>Forks</span>
+      </li>
+      <li>
+        <strong>34</strong>
+        <span>Issues Abertas</span>
+      </li>
+    </ul>
+  </RepositoryInfo>
+
+    <Issues>
+          <Link
+          to={`asdasdsad`}>
+            <div>
+              <strong>adasdsa</strong>
+              <p>asdsdaas</p>
+            </div>
+
+          <FiChevronRight size={20} />
+          </Link>
+
+    </Issues>
+  </>
+  );
 };
 
 export default Repository;
